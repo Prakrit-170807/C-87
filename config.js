d545b82ecb5ec49864e571337dfa26ec3039db77
Prakrit Maan <73429704+Prakrit-170807@users.noreply.github.com>
2022-01-16 12:21:51 +0530

    not working
    not working #HATEWHJ

---
+++ config.js
@@ -0,0 +1,11 @@
+export 
+const firebaseConfig = {
+    apiKey: "AIzaSyDQugfd8da0qIQOd9nlHqAHswFU8YDoIh0",
+    authDomain: "c-85-edb27.firebaseapp.com",
+    databaseURL: "https://c-85-edb27-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "c-85-edb27",
+    storageBucket: "c-85-edb27.appspot.com",
+    messagingSenderId: "831162770007",
+    appId: "1:831162770007:web:266337c53ad73b446b7912"
+  };
+
